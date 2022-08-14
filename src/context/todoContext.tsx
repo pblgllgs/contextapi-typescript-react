95ca89d587e65fa8c0589261cda901a28b917486
pblgllgs <pbl.gllgs@gmail.com>
2022-08-14 00:23:47 -0400

    context api typescript

---
+++ src/context/todoContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, FC, useState } from 'react';
+import { TodoContextType, ITodo } from '../@types/todo';
+
+export const TodoContext = createContext<TodoContextType | null>(null);
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const TodoProvider: FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: 1,
+      title: 'post 1',
+      description: 'this is a description',
+      status: false,
+    },
+    {
+      id: 2,
+      title: 'post 2',
+      description: 'this is a description',
+      status: true,
+    },
+  ]);
+  const saveTodo = (todo: ITodo) => {
+    const newTodo: ITodo = {
+      id: Math.random(), // not really unique - but fine for this example
+      title: todo.title,
+      description: todo.description,
+      status: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const updateTodo = (id: number) => {
+    todos.filter((todo: ITodo) => {
+      if (todo.id === id) {
+        todo.status = true;
+        setTodos([...todos]);
+      }
+    });
+  };
+  return (
+    <TodoContext.Provider value={{ todos, saveTodo, updateTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
